ca117f8cc5d4013224e2f0740a9cfd7d7c60ee70
Thae00 <mathaeoo97@gmail.com>
2021-12-06 21:58:09 +0630

    get productList with id

--- lib/screens/category_list.dart
+++ lib/screens/category_list.dart
@@ -11,6 +11,15 @@ class CategoryList extends StatefulWidget {
 class _CategoryListState extends State<CategoryList> {
   Stream<QuerySnapshot> categories =
       FirebaseFirestore.instance.collection("categories").snapshots();
+
+  //For Products
+  String defaultId = "17HG5bLQyTCyRYy8gpl1";
+  Future<QuerySnapshot<Map<String, dynamic>>> categoryProducts =
+      FirebaseFirestore.instance
+          .collection("products")
+          .where("category", isEqualTo: "17HG5bLQyTCyRYy8gpl1")
+          .get();
+  //
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -40,6 +49,24 @@ class _CategoryListState extends State<CategoryList> {
               ),
             );
           },
+        ),
+        // For products
+        SingleChildScrollView(
+          child: FutureBuilder(
+            future: categoryProducts,
+            builder: (context,
+                AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshots) {
+              return Container(
+                height: 220,
+                child: ListView.builder(
+                  itemCount: snapshots.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return Text("${snapshots.data!.docs[index]['name']}");
+                  },
+                ),
+              );
+            },
+          ),
         ),
       ],
     );
