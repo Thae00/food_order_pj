0be5c49617f9081964995d44a223b6239b30b3ff
Thae00 <mathaeoo97@gmail.com>
2021-12-25 20:40:49 +0630

    show location marker icon for admin

--- lib/screens/user_location.dart
+++ lib/screens/user_location.dart
@@ -7,24 +7,25 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class UserLocation extends StatefulWidget {
-
   double lat;
   double log;
 
-  UserLocation(this.lat,this.log);
+  UserLocation(this.lat, this.log);
 
   @override
   _UserLocationState createState() => _UserLocationState();
 }
 
 class _UserLocationState extends State<UserLocation> {
-
   bool loading = false;
 
   Completer<GoogleMapController> _controller = Completer();
   late CameraPosition initialCameraPosition;
   late Location location;
   late LocationData currentLocation;
+
+  Set<Marker> _marker = {};
+  late BitmapDescriptor locationIcon;
 
   @override
   void initState() {
@@ -34,24 +35,46 @@ class _UserLocationState extends State<UserLocation> {
 
   @override
   Widget build(BuildContext context) {
-    return this.loading? Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: initialCameraPosition,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-    ): Scaffold(body: CircularProgressIndicator(),);
+    return this.loading
+        ? Scaffold(
+            appBar: AppBar(title: Text("Customer's Location dddd"),),
+            body: GoogleMap(
+              markers: _marker,
+              mapType: MapType.normal,
+              initialCameraPosition: initialCameraPosition,
+              onMapCreated: (GoogleMapController controller) async {
+                _controller.complete(controller);
+                LatLng pinPosition = LatLng(widget.lat, widget.log);
+                setState(() {
+                  _marker.add(
+                    Marker(
+                      markerId: MarkerId("Icons"),
+                      position: pinPosition,
+                      icon: locationIcon,
+                    ),
+                  );
+                });
+              },
+            ),
+          )
+        : Scaffold(
+            body: CircularProgressIndicator(),
+          );
   }
 
   void setInitialLocation() async {
+    setMapIcon();
     initialCameraPosition = CameraPosition(
-      zoom: 18,
+      zoom: 16,
       target: LatLng(widget.lat, widget.log),
     );
     setState(() {
       this.loading = true;
     });
+  }
+
+  void setMapIcon() async {
+    locationIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), "assets/icons/map.png");
   }
 }
